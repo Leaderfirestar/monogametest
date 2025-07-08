344dc703621b758759990ea19a4f5342b4a662e0
leaderfirestar <EricAWebb2000@yahoo.com>
2025-07-08 18:28:47 -0500

    movement

--- Game1.cs
+++ Game1.cs
@@ -7,6 +7,8 @@ namespace monogametest;
 public class Game1 : Game
 {
     Texture2D ballTexture;
+    Vector2 ballPosition;
+    float ballSpeed;
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
 
@@ -20,7 +22,8 @@ public class Game1 : Game
     protected override void Initialize()
     {
         // TODO: Add your initialization logic here
-
+        ballPosition = new Vector2(_graphics.PreferredBackBufferWidth / 2, _graphics.PreferredBackBufferHeight / 2);
+        ballSpeed = 100f; // Set the ball speed
         base.Initialize();
     }
 
@@ -46,8 +49,32 @@ public class Game1 : Game
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
         // TODO: Add your drawing code here
+        float updatedBallSpeed = ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+        var kstate = Keyboard.GetState();
+
+        if (kstate.IsKeyDown(Keys.Up))
+        {
+            ballPosition.Y -= updatedBallSpeed;
+        }
+
+        if (kstate.IsKeyDown(Keys.Down))
+        {
+            ballPosition.Y += updatedBallSpeed;
+        }
+
+        if (kstate.IsKeyDown(Keys.Left))
+        {
+            ballPosition.X -= updatedBallSpeed;
+        }
+
+        if (kstate.IsKeyDown(Keys.Right))
+        {
+            ballPosition.X += updatedBallSpeed;
+        }
+
         _spriteBatch.Begin();
-        _spriteBatch.Draw(ballTexture, new Vector2(100, 100), Color.White);
+        _spriteBatch.Draw(ballTexture, ballPosition, null, Color.White, 0f, new Vector2(ballTexture.Width / 2, ballTexture.Height / 2), Vector2.One, SpriteEffects.None, 0f);
         _spriteBatch.End();
 
         base.Draw(gameTime);
