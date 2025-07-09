3ef59fb0a067fb9e7031f3681a450f668e16ba09
leaderfirestar <EricAWebb2000@yahoo.com>
2025-07-08 19:14:52 -0500

    followed tutorial to this part, getting Error CS0579 Duplicate 'global::System.Runtime.Versioning.TargetFrameworkAttribute'

--- Game1.cs
+++ Game1.cs
@@ -1,38 +1,28 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGameLibrary;
 
 namespace monogametest;
 
-public class Game1 : Game
+public class Game1 : Core
 {
-    Texture2D ballTexture;
-    Vector2 ballPosition;
-    float ballSpeed;
-    private GraphicsDeviceManager _graphics;
-    private SpriteBatch _spriteBatch;
 
-    public Game1()
+    public Game1() : base("MonoGame Test", 800, 600, false)
     {
-        _graphics = new GraphicsDeviceManager(this);
-        Content.RootDirectory = "Content";
-        IsMouseVisible = true;
     }
 
     protected override void Initialize()
     {
         // TODO: Add your initialization logic here
-        ballPosition = new Vector2(_graphics.PreferredBackBufferWidth / 2, _graphics.PreferredBackBufferHeight / 2);
-        ballSpeed = 100f; // Set the ball speed
         base.Initialize();
     }
 
     protected override void LoadContent()
     {
-        _spriteBatch = new SpriteBatch(GraphicsDevice);
 
         // TODO: use this.Content to load your game content here
-        ballTexture = Content.Load<Texture2D>("ball");
+        base.LoadContent();
     }
 
     protected override void Update(GameTime gameTime)
@@ -49,56 +39,6 @@ public class Game1 : Game
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
         // TODO: Add your drawing code here
-        float updatedBallSpeed = ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-
-        var kstate = Keyboard.GetState();
-
-        if (kstate.IsKeyDown(Keys.Up))
-        {
-            ballPosition.Y -= updatedBallSpeed;
-        }
-
-        if (kstate.IsKeyDown(Keys.Down))
-        {
-            ballPosition.Y += updatedBallSpeed;
-        }
-
-        if (kstate.IsKeyDown(Keys.Left))
-        {
-            ballPosition.X -= updatedBallSpeed;
-        }
-
-        if (kstate.IsKeyDown(Keys.Right))
-        {
-            ballPosition.X += updatedBallSpeed;
-        }
-
-        if (kstate.IsKeyDown(Keys.Right))
-        {
-            ballPosition.X += updatedBallSpeed;
-        }
-
-        if (ballPosition.X > _graphics.PreferredBackBufferWidth - ballTexture.Width / 2)
-        {
-            ballPosition.X = _graphics.PreferredBackBufferWidth - ballTexture.Width / 2;
-        }
-        else if (ballPosition.X < ballTexture.Width / 2)
-        {
-            ballPosition.X = ballTexture.Width / 2;
-        }
-
-        if (ballPosition.Y > _graphics.PreferredBackBufferHeight - ballTexture.Height / 2)
-        {
-            ballPosition.Y = _graphics.PreferredBackBufferHeight - ballTexture.Height / 2;
-        }
-        else if (ballPosition.Y < ballTexture.Height / 2)
-        {
-            ballPosition.Y = ballTexture.Height / 2;
-        }
-
-        _spriteBatch.Begin();
-        _spriteBatch.Draw(ballTexture, ballPosition, null, Color.White, 0f, new Vector2(ballTexture.Width / 2, ballTexture.Height / 2), Vector2.One, SpriteEffects.None, 0f);
-        _spriteBatch.End();
 
         base.Draw(gameTime);
     }
